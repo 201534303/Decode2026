98058c5042019f0e61e2f9a71a74c3aabf342664
ShiraListokin <26slistokin@gannacademy.org>
2025-11-15 12:27:34 -0500

    a
    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -1,28 +1,43 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Drivetrain {
 
-    DcMotor frontLeft, frontRight, backLeft, backRight;
+    protected DcMotor frontLeft, frontRight, backLeft, backRight;
+    protected IMU imu;
+    private IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+            RevHubOrientationOnRobot.LogoFacingDirection.UP,
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+    protected Telemetry telemetry;
 
-    public Drivetrain(HardwareMap hardwareMap){
+    public Drivetrain(HardwareMap hardwareMap, Telemetry t){
         frontLeft = hardwareMap.get(DcMotor.class, "drivefl");
         frontRight = hardwareMap.get(DcMotor.class, "drivefr");
         backLeft = hardwareMap.get(DcMotor.class, "drivebl");
         backRight = hardwareMap.get(DcMotor.class, "drivebr");
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        telemetry = t;
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
     }
 
-    public void driveRobot(double drive, double strafe, double turn){
-
+    protected void driveRobot(double drive, double strafe, double turn){
         frontLeft.setPower((drive+strafe+turn)*-1);
         frontRight.setPower((drive-strafe-turn)*-1);
         backLeft.setPower((drive-strafe+turn)*-1);
         backRight.setPower((drive+strafe-turn)*-1);
     }
+
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DrivetrainTele.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class DrivetrainTele extends Drivetrain{
+
+    private Gamepad gamepad1, gamepad2;
+
+
+    public DrivetrainTele(HardwareMap hardwareMap, Gamepad g1, Gamepad g2, Telemetry t) {
+        super(hardwareMap, t);
+        gamepad1 = g1;
+        gamepad2 = g2;
+    }
+
+    public void feildCentricDrive(){
+        double yMove = -gamepad1.left_stick_y; //Y stick value is reversed
+        double xMove = gamepad1.left_stick_x;
+        double rot = gamepad1.right_stick_x;
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        // Rotate the movement direction counter to the bot's rotation
+        double rotedX = xMove * Math.cos(-botHeading) - yMove * Math.sin(-botHeading);
+        double rotedY = xMove * Math.sin(-botHeading) + yMove * Math.cos(-botHeading);
+
+        rotedX = rotedX * 1.1;  // Counteract imperfect strafing
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(rotedY) + Math.abs(rotedX) + Math.abs(rot), 1);
+        double frontLeftPower = (rotedY + rotedX + rot) / denominator;
+        double backLeftPower = (rotedY - rotedX + rot) / denominator;
+        double frontRightPower = (rotedY - rotedX - rot) / denominator;
+        double backRightPower = (rotedY + rotedX - rot) / denominator;
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+    }
+}
