baaa173f07326e6deeea243c728ba46cfdc75d29
ShiraListokin <26slistokin@gannacademy.org>
2025-12-21 15:28:52 -0500

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterTele.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.subsystems.supperClasses.Shooter;
+import org.firstinspires.ftc.teamcode.subsystems.superClasses.Shooter;
 
 public class ShooterTele extends Shooter {
     private Gamepad gamepad1, gamepad2;
