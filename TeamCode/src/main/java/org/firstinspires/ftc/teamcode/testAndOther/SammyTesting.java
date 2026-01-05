8a473d1874f4f4c6c1473a23c90e0b93d806b679
Beatriz Almeida <beatriz@fornasaro.com>
2026-01-05 08:02:22 -0500

    testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAndOther/SammyTesting.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.testAndOther;
+
+public class SammyTesting {
+    public static double x = 0;
+    public static double y = 0;
+    public static double z = 0;
+}
