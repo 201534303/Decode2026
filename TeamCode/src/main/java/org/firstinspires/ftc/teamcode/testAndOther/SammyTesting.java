63b90d32815e26ccf93f335081b9903ec6ed7b46
Beatriz Almeida <beatriz@fornasaro.com>
2026-01-05 08:03:50 -0500

    testing changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAndOther/SammyTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAndOther/SammyTesting.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode.testAndOther;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config
+@TeleOp(name = "Sammy Shooter Testing")
+
 public class SammyTesting {
     public static double x = 0;
     public static double y = 0;
