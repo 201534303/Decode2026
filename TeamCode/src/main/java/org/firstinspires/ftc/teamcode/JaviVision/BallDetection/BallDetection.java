4677033fe146c399782d277745bdf6c42f3dca86
kijavnash <72349338+kijavnash@users.noreply.github.com>
2026-02-11 07:41:11 -0500

    c

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaviVision/BallDetection/BallDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaviVision/BallDetection/BallDetection.java
@@ -15,24 +15,29 @@ public class BallDetection {
 
     public BallDetection(HardwareMap hardwareMap) {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(0);
+        limelight.pipelineSwitch(0); // update Ball pipeline
+        //limelight.pipelineSwitch(4); // HSVTuner pipeline
         limelight.start();
     }
 
     // ------------------------------------------------------------
-    public double[] update() {
+    public double[] updateBall() {
         // ================= LIMELIGHT =================
-<<<<<<< Updated upstream
-        double[] inputs = {2, 0, 0, 0, 255, 255, 255, 0};
-=======
         double[] purple = {173, 109, 0, 8, 255, 107};
         double purpleH = 100000 + purple[0]*1000 + purple[3];
         double purpleS = 100000 + purple[1]*1000 + purple[4];
         double purpleV = 100000 + purple[2]*1000 + purple[5];
         double[] green = {};
-        double[] inputs = {2, purpleH, purpleS, purpleV, 1, 0, 0, 0};
+        double[] inputs = {1, purpleH, purpleS, purpleV, 1, 0, 0, 0};
         //                 P, Hmin, Smin, Vmin, Hmax, Smax, vmax, 0
->>>>>>> Stashed changes
+        limelight.updatePythonInputs(inputs);
+        LLResult result = limelight.getLatestResult();
+        return result.getPythonOutput();
+    }
+    public double[] updateHSV() {
+        // ================= LIMELIGHT =================
+        double[] inputs = {1, 0, 0, 0, 0, 0, 0, 0};
+        //                 P, Hmin, Smin, Vmin, Hmax, Smax, vmax, 0
         limelight.updatePythonInputs(inputs);
         LLResult result = limelight.getLatestResult();
         return result.getPythonOutput();
