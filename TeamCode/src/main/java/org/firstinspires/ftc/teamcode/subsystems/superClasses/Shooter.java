e11313614e487f96f366c6bf08e5a0d9b1f29467
Shira <113054527+ShiraListokin@users.noreply.github.com>
2026-02-23 14:48:36 -0500

    bang bang implemented
    woooooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/superClasses/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/superClasses/Shooter.java
@@ -76,14 +76,14 @@ public class Shooter {
     }
 
     public void flywheelSpin(double targetVelo, double currentVelo, double kf){//kf is a tester varible
-        double speed = PIDF(targetVelo-currentVelo, targetVelo, 12,0,0.1,0.59);
-        //shooterR.setVeloCoefficients(12, 0, 0.12);
-        //shooterL.setVeloCoefficients(12, 0, 0.12);
-        //shooterR.setFeedforwardCoefficients(0, 2.4, 0);
-        //shooterL.setFeedforwardCoefficients(0, 2.4, 0);
-        //12, 0.59 13
-        shooterR.setVelocity(speed);
-        shooterL.setVelocity(speed);
+        double speed;
+        if (targetVelo-currentVelo <= 0){
+            speed = 0;
+        } else {
+            speed = 1;
+        }
+        shooterL.set(speed);
+        shooterR.set(speed);
         telemetry.addData("target velocity", Math.round(targetVelo*100)/100.0);
         telemetry.addData("current velocity", Math.round(currentVelo*100)/100.0);
     }
