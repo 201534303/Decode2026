213a9b4d450e5ef65ab1ae4df996999f41dcaa50
kijavnash <72349338+kijavnash@users.noreply.github.com>
2026-02-26 06:41:53 -0500

    Update positioncaller.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaviVision/BallDetection/positioncaller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JaviVision/BallDetection/positioncaller.java
@@ -30,19 +30,15 @@ public class positioncaller extends OpMode {
             double z = results[3];
             double raw_yaw = results[4];
             double tx = results[6];
-            double heading = 90
-            double theta = yaw - tx;
             double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(z,2));
             telemetry.addData("X: ", x);
             telemetry.addData("Y:", y);
             telemetry.addData("Z", z);
             telemetry.addData("Raw Yaw: ", raw_yaw);
-            telemetry.addData("Yaw", yaw);
             telemetry.addData("Tx", tx);
             telemetry.addData("Distance: ", distance);
-            telemetry.addData("Theta", theta);
-            telemetry.addData("X (cos):", distance*Math.cos(Math.toRadians(theta)));
-            telemetry.addData("Z (sin):",  distance*Math.sin(Math.toRadians(theta)));
+            //telemetry.addData("X (cos):", distance*Math.cos(Math.toRadians(theta)));
+            //telemetry.addData("Z (sin):",  distance*Math.sin(Math.toRadians(theta)));
         }
         telemetry.update();
         dash.update();
