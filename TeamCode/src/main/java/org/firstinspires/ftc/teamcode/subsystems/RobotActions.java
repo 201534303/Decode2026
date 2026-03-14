2b505e840389242c33526c7de0f000b0684677e6
JGcode126 <2008jerrygong@gmail.com>
2026-03-13 22:14:18 -0400

    meanwhile the lazer robotics DOOO DOO DODODODOOOOO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RobotActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RobotActions.java
@@ -452,11 +452,11 @@ public class RobotActions {
             speed = 3.63909*dist+1114.64786;
         }
         else if(dist > 98){
-            hood = 0.15;
+            hood = 0.25;
             speed = 1096.99182 + 3.05*dist;
             // 2.7835
         }
-        else if(dist > 55){ // close5.84356\cdot0.968317^{x}
+        else if(dist > 60){ // close5.84356\cdot0.968317^{x}
             hood = 5.84356*Math.pow(0.968317, dist);
             speed = 5*dist+860;
         }
