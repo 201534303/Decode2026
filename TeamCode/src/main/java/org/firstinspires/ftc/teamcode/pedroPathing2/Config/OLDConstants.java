a632eecd50953a6e9c3a9c99f3ab62f55e8c33f7
Shira <113054527+ShiraListokin@users.noreply.github.com>
2026-04-11 23:09:41 -0400

    gradle update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing2/Config/OLDConstants.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.pedroPathing2.Config;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Compatibility shim for older Pedro Pathing autos that still import the removed
+ * pedroPathing2 Config package.
+ */
+public final class OLDConstants {
+    private OLDConstants() {}
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return org.firstinspires.ftc.teamcode.pedroPathing.Constants.createFollower(hardwareMap);
+    }
+}
