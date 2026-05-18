b80c2f876080528a05897d0f7ba9a9233414c358
RV-1234 <ria.vartak@gmail.com>
2026-05-18 19:14:18 -0400

    enabled testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testers/ShootingTeleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/testers/ShootingTeleOpTesting.java
@@ -26,7 +26,7 @@ import java.util.concurrent.TimeUnit;
 
 @TeleOp(name="Shooter Testing", group="Iterative OpMode")
 @Config
-@Disabled
+//@Disabled
 public class ShootingTeleOpTesting extends OpMode {
 
     //choose
