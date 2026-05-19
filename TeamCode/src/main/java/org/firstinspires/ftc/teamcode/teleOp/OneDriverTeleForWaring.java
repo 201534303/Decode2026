1f1b520228b33b3d57c1f6db5260d65e5ac6af99
Beatriz Almeida <beatriz@fornasaro.com>
2026-05-18 23:29:59 -0400

    deleting unused files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/OneDriverTeleForWaring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/OneDriverTeleForWaring.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleOp;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Paths.OLD.OLDChoose.Alliance.BLUE;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Paths.OLD.OLDChoose.Alliance.RED;
+import static org.firstinspires.ftc.teamcode.pedroPathing.OLDChoose.Alliance.BLUE;
+import static org.firstinspires.ftc.teamcode.pedroPathing.OLDChoose.Alliance.RED;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.JaviVision.Position.FinalPositionV3.LimelightProcessor_v3Tele;
 import org.firstinspires.ftc.teamcode.pedroPathing.Config.Constants;
-import org.firstinspires.ftc.teamcode.pedroPathing.Paths.OLD.OLDChoose;
+import org.firstinspires.ftc.teamcode.pedroPathing.OLDChoose;
 import org.firstinspires.ftc.teamcode.subsystems.RobotActions;
 import org.firstinspires.ftc.teamcode.subsystems.superClasses.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.superClasses.Intake;
